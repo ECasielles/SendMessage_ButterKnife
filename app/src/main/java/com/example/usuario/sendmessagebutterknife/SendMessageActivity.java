ae2a79cd8e1e0840192eedaa7dccd5dafbc3b99e
ECasielles <flapspoiler@gmail.com>
2017-10-16 13:26:39 +0200

    Fixed

--- app/src/main/java/com/example/usuario/sendmessagebutterknife/SendMessageActivity.java
+++ app/src/main/java/com/example/usuario/sendmessagebutterknife/SendMessageActivity.java
@@ -10,10 +10,16 @@ import android.widget.EditText;
 
 import com.example.usuario.sendmessagebutterknife.pojo.Message;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Activity principal. Pide un nombre de usuario
  * y un mensaje para enviarlo a la Activity ViewMessage,
  * que se lanza al pulsar el botón "OK".
+ *
+ * Versión con la librería ButterKnife
  *
  * Conceptos:
  * <ul>
@@ -23,41 +29,32 @@ import com.example.usuario.sendmessagebutterknife.pojo.Message;
  * </ul>
  *
  * @author Enrique Casielles Lapeira
- * @version 1.0
+ * @version 2.0
  * @see android.app.Activity
  * @see android.support.v7.app.AppCompatActivity
  *
  */
 public class SendMessageActivity extends AppCompatActivity {
 
-    public static final String TAG = "com.example.sendmessagebutterknife";
-    //public static final String TAG = "SendMessageActivity";
-    private EditText edtMessage;
-    private EditText edtUser;
-    private Button btnOk;
+    @BindView(R.id.edtMessage)
+    EditText edtMessage;
+    @BindView(R.id.edtUser)
+    EditText edtUser;
+    @BindView(R.id.btnOk)
+    Button btnOk;
+
+
+    //public static final String TAG = "com.example.sendmessagebutterknife";
+    public static final String TAG = "sendmessagebutterknife";
+    private Message mensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
-        edtMessage = (EditText) findViewById(R.id.edtMessage);
-        edtUser = (EditText) findViewById(R.id.edtUser);
-        btnOk = (Button) findViewById(R.id.btnOk);
-        //Usando clases anónimas
-        btnOk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                Message message = new Message(
-                        edtMessage.getText().toString(),
-                        edtUser.getText().toString()
-                );
-                bundle.putParcelable("message", message);
-                Intent intent = new Intent(SendMessageActivity.this, ViewMessageActivity.class);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
+
+        //Vincular el ide de las vistas en el XMl con las clases View
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -84,27 +81,22 @@ public class SendMessageActivity extends AppCompatActivity {
         Log.d(TAG, "SendMessage.onPause()");
     }
 
+
+    @OnClick(R.id.btnOk)
     public void getOnClick(View view) {
-
-        /*switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btnOk:
-                //1: Recoger el mensaje
-                //2: Crear un objeto Bundle y añadir el mensaje
                 Bundle bundle = new Bundle();
-                bundle.putString("message", edtMessage.getText().toString());
-                bundle.putString("user", edtUser.getText().toString());
-
-                //3: Crear un objeto Intent
-                //Por nomenclatura se usa el nombre de la clase más this, aunque
-                //sea obvio.
+                Message message = new Message(
+                        edtMessage.getText().toString(),
+                        edtUser.getText().toString()
+                );
+                bundle.putParcelable("message", message);
                 Intent intent = new Intent(SendMessageActivity.this, ViewMessageActivity.class);
-
-                //4: Añadir el objeto Bundle al objeto Intent
                 intent.putExtras(bundle);
-                //4: Iniciar la Activity ViewMessage
                 startActivity(intent);
                 break;
-        }*/
-
+        }
     }
+
 }
